e591795dc8fd643de5365dace30de4d394bcfca3
Volha Patsiahevich <zaprudskaya@gmail.com>
2025-02-03 09:29:29 +0100

    feat: add pagination

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -5,14 +5,16 @@ import Main from './components/Main/Main';
 import Spinner from './components/Spinner/Spinner';
 import ErrorButton from './components/ErrorButton/ErrorButton';
 import { ENDPOINTS, ServerResponse } from './utils/types';
+import Pagination from './components/Pagination/Pagination';
 
 function App() {
   const [data, setData] = useState<null | ServerResponse>(null);
   const [isLoaded, setIsLoaded] = useState(true);
+  const [page, setPage] = useState(1);
 
   async function fetchData(searchValue: string) {
     setIsLoaded(false);
-    const baseLink = `https://swapi.dev/api/${ENDPOINTS.People}/?search=${searchValue}`;
+    const baseLink = `https://swapi.dev/api/${ENDPOINTS.People}?page=${page}&search=${searchValue}`;
 
     try {
       const response = await fetch(baseLink, { method: 'GET' });
@@ -29,13 +31,21 @@ function App() {
   useEffect(() => {
     const savedValue = localStorage.getItem('searchValue') ?? '';
     fetchData(savedValue);
-  }, []);
+  }, [page]);
 
   return (
     <div className="app">
       <ErrorButton />
       <Header onSearchApply={fetchData} />
       {isLoaded ? <Main currentData={data} /> : <Spinner />}
+      {data ? (
+        <Pagination
+          currentPage={page}
+          changePage={setPage}
+          prevPage={data?.previous}
+          nextPage={data?.next}
+        />
+      ) : null}
     </div>
   );
 }
---
+++ react-project/src/components/Pagination/Pagination.tsx
@@ -0,0 +1,39 @@
+import { Dispatch, SetStateAction } from 'react';
+import './Pagination.scss';
+
+type PaginationProps = {
+  nextPage: null | undefined | string;
+  prevPage: null | undefined | string;
+  currentPage: number;
+  changePage: Dispatch<SetStateAction<number>>;
+};
+function Pagination(props: PaginationProps) {
+  function nextPage() {
+    props.changePage((prev: number) => (prev += 1));
+  }
+
+  function prevPage() {
+    props.changePage((prev: number) => (prev -= 1));
+  }
+  return (
+    <nav className="pagination">
+      <button
+        className="prev"
+        onClick={prevPage}
+        disabled={!props.prevPage ? true : false}
+      >
+        prev
+      </button>
+      <p className="current">{props.currentPage}</p>
+      <button
+        className="next"
+        onClick={nextPage}
+        disabled={!props.nextPage ? true : false}
+      >
+        next
+      </button>
+    </nav>
+  );
+}
+
+export default Pagination;
